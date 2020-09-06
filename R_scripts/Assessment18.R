24802a7e09da88f2c33b0799d2978f68d41b9b2b
Anizet <arnaudnizet@hotmail.com>
2020-09-06 12:38:00 +0200

    pouloulou2

--- R_scripts/Assessment18.R
+++ R_scripts/Assessment18.R
@@ -28,3 +28,46 @@ lse <- replicate(B, {
 })
 
 lse <- data.frame(beta_0 = lse[1,], beta_1 = lse[2,]) 
+
+rss <- function(beta0, beta1, data){
+  resid <- galton_heights$son - (beta0+beta1*galton_heights$father)
+  return(sum(resid^2))
+}
+
+
+
+beta1 = seq(0, 1, len=nrow(galton_heights))
+results <- data.frame(beta1 = beta1,
+                      rss = sapply(beta1, rss, beta0 = 36))
+results %>% ggplot(aes(beta1, rss)) + geom_line() + 
+  geom_line(aes(beta1, rss), col=2)
+
+set.seed(1989) #if you are using R 3.5 or earlier
+library(HistData)
+data("GaltonFamilies")
+options(digits = 3)    # report 3 significant digits
+
+female_heights <- GaltonFamilies %>%     
+  filter(gender == "female") %>%     
+  group_by(family) %>%     
+  sample_n(1) %>%     
+  ungroup() %>%     
+  select(mother, childHeight) %>%     
+  rename(daughter = childHeight)
+
+xi <- lm(mother ~ daughter, data = female_heights)
+summary(xi)
+
+44.18 + 0.3103*female_heights$daughter[1]
+female_heights$mother[1]
+
+library(Lahman)
+bat_02 <- Batting %>% filter(yearID == 2002) %>%
+  mutate(pa = AB + BB, singles = (H - X2B - X3B - HR)/pa, bb = BB/pa) %>%
+  filter(pa >= 100) %>%
+  select(playerID, singles, bb)
+
+bat_9901 <- Batting %>% filter(yearID %in% 1999:2001) %>%
+  mutate(pa = AB + BB, singles = (H - X2B - X3B - HR)/pa, bb = BB/pa) %>%
+  filter(pa >= 100) %>%
+  select(playerID, singles, bb)
